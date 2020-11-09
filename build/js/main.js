24326d685b1a9a5c857ea0f5c38befebe296cf4b
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2020-11-09 17:46:20 +0500

    Добавляет стили в шапку

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+(function () {
+  var headerContainer = document.querySelector('.page-header');
+  var mainNav = headerContainer.querySelector('.main-nav');
+  var mainMenuToggle = headerContainer.querySelector('.main-menu-toggle');
+
+  mainMenuToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('main-nav--closed');
+    mainNav.classList.toggle('main-nav--opened');
+    headerContainer.classList.toggle('page-header--bg')
+  })
+})();
