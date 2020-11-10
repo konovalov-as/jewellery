f24a34670e4a614154266ba336aab048158bc509
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2020-11-10 11:52:59 +0500

    Добавляет стили подвала

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function () {
+  // menu toggle
   var headerContainer = document.querySelector('.page-header');
   var mainNav = headerContainer.querySelector('.main-nav');
   var mainMenuToggle = headerContainer.querySelector('.main-menu-toggle');
@@ -9,5 +10,35 @@
     mainNav.classList.toggle('main-nav--closed');
     mainNav.classList.toggle('main-nav--opened');
     headerContainer.classList.toggle('page-header--bg')
-  })
+  });
+
+  // hide social labels
+  var socialLabels = document.querySelectorAll('.social__list .social__label');
+  function hideLabels(action) {
+    for (var i = 0; i < socialLabels.length; i++) {
+      if (action === 'add') {
+        socialLabels[i].classList.add('visually-hidden');
+      }
+      if (action === 'remove') {
+        socialLabels[i].classList.remove('visually-hidden');
+      }
+    }
+  }
+
+  if (document.documentElement.clientWidth < 768) {
+    hideLabels('remove');
+  }
+
+  var onresize = function () {
+    if (document.documentElement.clientWidth < 768) {
+      hideLabels('add');
+    }
+    if (document.documentElement.clientWidth > 768) {
+      hideLabels('remove');
+    }
+  }
+  window.addEventListener("resize", onresize);
+
+  //
+
 })();
