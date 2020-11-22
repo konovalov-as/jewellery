e904f7674783ca7b6e26f32bb2ef2b47e63d117a
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2020-11-22 14:39:07 +0500

    Подключает swiper

--- build/js/main.js
+++ build/js/main.js
@@ -184,4 +184,21 @@
   var singUpEmail = signUpForm.querySelector('.sign-up-form input');
   singUpEmail.value = localStorage.getItem('emailInput');
 
+  // slider
+  var swiper = new Swiper('.new__slider-content', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    loop: true,
+    // loopFillGroupWithBlank: true,
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+    navigation: {
+      nextEl: '.new__slider-next-button',
+      prevEl: '.new__slider-prev-button',
+    },
+  });
+
 })();
