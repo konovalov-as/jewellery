0264d69551c13ac123a2fb2d51637c5e4ac00347
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2020-11-24 17:01:02 +0500

    Добавляет изменения в подключение слайдера

--- build/js/vendor.js
+++ build/js/vendor.js
@@ -9563,29 +9563,32 @@
 /* stylelint-disable */
 
 // slider
-var swiper = new Swiper('.new__slider-content', {
-  slidesPerView: 2,
-  spaceBetween: 30,
-  slidesPerGroup: 2,
-  loop: true,
-  breakpoints: {
-    1024: {
-      slidesPerView: 4,
-      slidesPerGroup: 4,
+var sliderContainer = document.querySelector('.new__slider-content');
+if (sliderContainer) {
+  var swiper = new Swiper('.new__slider-content', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 2,
+    loop: true,
+    breakpoints: {
+      1024: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+      },
     },
-  },
-  // loopFillGroupWithBlank: true,
-  pagination: {
-    el: '.new__slider-count',
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<button class="' + className + '" aria-label="Slide ' + (index + 1) + '">' + (index + 1) + '</button>';
+    // loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.new__slider-count',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<button class="' + className + '" aria-label="Slide ' + (index + 1) + '">' + (index + 1) + '</button>';
+      },
     },
-  },
-  navigation: {
-    nextEl: '.new__slider-next-button',
-    prevEl: '.new__slider-prev-button',
-  },
-});
+    navigation: {
+      nextEl: '.new__slider-next-button',
+      prevEl: '.new__slider-prev-button',
+    },
+  });
 
-swiper.slideNext();
+  swiper.slideNext();
+}
