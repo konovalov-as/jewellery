c9b13fd70792ce6e90655435a1be9ebf5ccb6602
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2020-11-25 18:01:36 +0500

    Perfect Pixel mobile

--- build/js/vendor.js
+++ build/js/vendor.js
@@ -9571,19 +9571,42 @@ if (sliderContainer) {
     slidesPerGroup: 2,
     loop: true,
     breakpoints: {
+      0: {
+        pagination: {
+          el: '.new__slider-count',
+          clickable: true,
+          type: 'custom',
+          renderCustom: function (swiper, current, total) {
+            // return '<span>'+ (index + 1) + '</span>' + ' of ' + '<span>' + (className) + '</span>';
+            // return '<span>'+ total + '</span>' + ' of ' + '<span>' + totalClass + '</span>';
+            // return '<span class="' + bulletActiveClass + '">' + total + '</span>' + ' of ' + '<span class="' + bulletClass + '">' + totalClass + '</span>';
+            // return '<button class="' + className + '" aria-label="Slide ' + (index + 1) + '">' + (index + 1) + '</button>';
+            return current + ' of ' + total;
+          },
+        },
+      },
+      768: {
+        pagination: {
+          el: '.new__slider-count',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<button class="' + className + '" aria-label="Slide ' + (index + 1) + '">' + (index + 1) + '</button>';
+          },
+        },
+      },
       1024: {
         slidesPerView: 4,
         slidesPerGroup: 4,
+        pagination: {
+          el: '.new__slider-count',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<button class="' + className + '" aria-label="Slide ' + (index + 1) + '">' + (index + 1) + '</button>';
+          },
+        },
       },
     },
     // loopFillGroupWithBlank: true,
-    pagination: {
-      el: '.new__slider-count',
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<button class="' + className + '" aria-label="Slide ' + (index + 1) + '">' + (index + 1) + '</button>';
-      },
-    },
     navigation: {
       nextEl: '.new__slider-next-button',
       prevEl: '.new__slider-prev-button',
